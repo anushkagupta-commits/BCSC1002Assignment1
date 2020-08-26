f9afb474fc364f4547deadf238e1fc20c4ecc5b2
Anushka Gupta <anushka.gupta_cs19@gla.ac.in>
2020-08-27 01:37:26 +0530

    add setter methods for the Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -15,11 +15,23 @@ public class Book {
         return bookName;
     }
 
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
     public String getAuthorName() {
         return authorName;
     }
 
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
     public String getIsbnNumber() {
         return isbnNumber;
+    }
+
+    public void setIsbnNumber(String isbnNumber) {
+        this.isbnNumber = isbnNumber;
     }
 }
