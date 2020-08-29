1335b42edaf8e6db61f916772b829031384fdefa
Anushka Gupta <anushka.gupta_cs19@gla.ac.in>
2020-08-30 02:00:40 +0530

    create getter and setter methods for Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -25,6 +25,62 @@ public class Student {
     }
 
     public Student() {
-        this.names = new Book[10];
+        this.names = new Book[30];
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public long getUniversityRollNo() {
+        return universityRollNo;
+    }
+
+    public void setUniversityRollNo(long universityRollNo) {
+        this.universityRollNo = universityRollNo;
+    }
+
+    public int bookIssued() {
+        return bookIssued;
+    }
+
+    public void setBookIssued(int bookIssued) {
+        this.bookIssued = bookIssued;
+    }
+
+    public Book[] getNames() {
+        return names.clone();
+    }
+
+    public void setNames(Book[] names) {
+        this.names = names;
     }
 }
